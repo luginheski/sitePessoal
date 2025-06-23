3d3a2261fb92b70e2a058366f19cad516299d670
luginheski <rodrigo514_2@hotmail.com>
2025-06-23 14:46:13 -0300

    Ajuste Model
    Arrumado janela poup, substituido por modal

--- scrypt.js
+++
@@ -1,7 +0,0 @@
-function mostrarPopUp() {
-    document.getElementById("meuPopUp").style.display = "block";
-  }
-
-  function esconderPopUp() {
-    document.getElementById("meuPopUp").style.display = "none";
-  }
